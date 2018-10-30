6db8796a0206019083a34b6a4a672b62f907cbad
Giel van Schijndel <giel@mortis.eu>
2018-10-30 18:47:42 +0100

    groovy: return API object

--- vars/getCiDriver.groovy
+++ vars/getCiDriver.groovy
@@ -13,6 +13,68 @@
  * limitations under the License.
  */
 
+class CiDriver
+{
+  private modulepath
+  private cmd
+  private steps
+  private prerequisites_installed
+
+  CiDriver(steps, modulepath, workspace) {
+    this.steps = steps
+    this.modulepath = modulepath
+    this.cmd = "PYTHONPATH=\"${modulepath}\" python -m cidriver --config=\"${workspace}/cfg.yml\" --workspace=\"${workspace}\""
+    this.prerequisites_installed = false
+  }
+
+  public def install_prerequisites() {
+    if (!this.prerequisites_installed) {
+      steps.sh(script: 'pip install --user \'click>=7.0,<8.0\'')
+      this.prerequisites_installed = true
+    }
+  }
+
+  public def build() {
+    this.install_prerequisites()
+
+    /*
+     * We're splitting the enumeration of phases and variants from their execution in order to
+     * enable Jenkins to execute the different variants within a phase in parallel.
+     */
+    def phases = steps.sh(
+        script: "${this.cmd} phases",
+        returnStdout: true,
+      ).split("\\r?\\n")
+
+    phases.each { phase ->
+        def variants = steps.sh(
+            script: "${this.cmd} variants --phase=\"${phase}\"",
+            returnStdout: true,
+          ).split("\\r?\\n")
+        steps.stage(phase) {
+          def stepsForBuilding = variants.collectEntries { variant ->
+            [ "${phase}-${variant}": {
+              steps.stage("${phase}-${variant}") {
+                steps.sh(script: "${this.cmd} build --phase=\"${phase}\" --variant=\"${variant}\"")
+                if (phase == 'upload')
+                {
+                  def meta = steps.readJSON(text: steps.sh(
+                      script: "${this.cmd} getinfo --phase=\"${phase}\" --variant=\"${variant}\"",
+                      returnStdout: true,
+                    ))
+                  if (meta.containsKey('ivy-output-dir')) {
+                    steps.stashPublishedArtifactsFiles(variant, meta['ivy-output-dir'])
+                  }
+                }
+              }
+            }]
+          }
+          steps.parallel stepsForBuilding
+        }
+    }
+  }
+}
+
 /**
   * getCiDriver()
   *
@@ -37,5 +99,5 @@ def call(version) {
           relativeTargetDir: cidriver,
         ]],
     ])
-  return "PYTHONPATH=\"${cidriver}\" python -m cidriver"
+  return new CiDriver(this, cidriver, WORKSPACE)
 }
