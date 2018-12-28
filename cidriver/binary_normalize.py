b47e70c043d28a89c80a00d4b14d86ad066cbe88
Giel van Schijndel <giel@mortis.eu>
2018-12-28 14:08:44 +0100

    Add monkey patch to reproduce TAR before and after Python 3.5.2
    Specifically the path splitting code changed in that version because of
    https://bugs.python.org/issue24838. This doesn't solve that bug for
    older Python versions but is a reimplementation of the same older method
    that's non-greedy instead of greedy. This ensures that archives produced
    with these older versions are binary equal to those from 3.5.2 on.

--- cidriver/binary_normalize.py
+++ cidriver/binary_normalize.py
@@ -1,6 +1,35 @@
 from gzip import GzipFile
 import os
 import tarfile
+import sys
+
+if sys.version_info < (3,5,2):
+    class TarInfoWithoutGreedyNameSplitting(tarfile.TarInfo):
+        """Variant of tarfile.TarInfo that helps to ensure reproducible builds."""
+
+        def _posix_split_name(self, name, *_):
+            """Split a path into a prefix and a name part.
+
+            This is a non-greedy variant of this function for Python versions older than 3.5.2.
+
+            This ensures that archives before and after that version are equal at the byte level.
+            This change is necessary due to the fix for https://bugs.python.org/issue24838
+            """
+            prefix = name[:-tarfile.LENGTH_NAME]
+            while prefix and prefix[-1] != "/" and len(prefix) < len(name):
+                prefix = name[:len(prefix)+1]
+
+            name = name[len(prefix):]
+            prefix = prefix[:-1]
+
+            if len(name) > tarfile.LENGTH_NAME:
+                raise ValueError("path is too long")
+            return prefix, name
+
+    class TarFile(tarfile.TarFile):
+        tarinfo = TarInfoWithoutGreedyNameSplitting
+else:
+    TarFile = tarfile.TarFile
 
 def normalize(filename, source_date_epoch=0):
     """Make the given file as close to reproducible as possible. Mostly be clamping timestamps to source_date_epoch."""
@@ -8,11 +37,11 @@ def normalize(filename, source_date_epoch=0):
         return
 
     if filename.endswith('.tar') or filename.endswith('.tar.gz'):
-        with tarfile.open(filename) as in_archive, open(filename + '.tmp', 'wb') as outfile:
+        with TarFile.open(filename) as in_archive, open(filename + '.tmp', 'wb') as outfile:
             if filename.endswith('.gz'):
                 outfile = GzipFile(filename=filename, mode='wb', compresslevel=9, fileobj=outfile, mtime=source_date_epoch)
             try:
-                with tarfile.open('', fileobj=outfile, format=tarfile.USTAR_FORMAT, mode='w', encoding='UTF-8') as out_archive:
+                with TarFile.open('', fileobj=outfile, format=tarfile.USTAR_FORMAT, mode='w', encoding='UTF-8') as out_archive:
                     # Sorting the file list ensures that we don't depend on the order that files appear on disk
                     for member in sorted(in_archive, key=lambda x: x.name):
                         # Clamping mtime to source_date_epoch ensures that source files are the only sources of timestamps, not build time
