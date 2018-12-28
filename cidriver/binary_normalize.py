5501200e4793c26ad3c9ca749d45456c53713d6c
Giel van Schijndel <giel@mortis.eu>
2018-12-28 18:15:47 +0100

    Allow binary normalization to be called recursively
    This allows normalization of archives contained in archives too.

--- cidriver/binary_normalize.py
+++ cidriver/binary_normalize.py
@@ -1,5 +1,6 @@
 from gzip import GzipFile
 import os
+import shutil
 import tarfile
 import sys
 
@@ -31,17 +32,24 @@ if sys.version_info < (3,5,2):
 else:
     TarFile = tarfile.TarFile
 
-def normalize(filename, source_date_epoch=0):
+def normalize(filename, fileobj=None, outname='', outfileobj=None, source_date_epoch=0):
     """Make the given file as close to reproducible as possible. Mostly be clamping timestamps to source_date_epoch."""
-    if not os.path.isfile(filename):
+    if (fileobj is None or outfileobj is None) and not os.path.isfile(filename):
         return
 
     if filename.endswith('.tar') or filename.endswith('.tar.gz'):
-        with TarFile.open(filename) as in_archive, open(filename + '.tmp', 'wb') as outfile:
-            if filename.endswith('.gz'):
-                outfile = GzipFile(filename='', mode='wb', compresslevel=9, fileobj=outfile, mtime=source_date_epoch)
+        if outfileobj is None:
+            archivefile = outfile = open(filename + '.tmp', 'wb')
+        else:
+            archivefile = outfile = outfileobj
+        with TarFile.open(filename, fileobj=fileobj) as in_archive:
             try:
-                with TarFile.open('', fileobj=outfile, format=tarfile.USTAR_FORMAT, mode='w', encoding='UTF-8') as out_archive:
+                compress = False
+                if filename.endswith('.gz'):
+                    compress = True
+                    archivefile = GzipFile(filename=outname, mode='wb', compresslevel=9, fileobj=outfile, mtime=source_date_epoch)
+
+                with TarFile.open(outname, fileobj=archivefile, format=tarfile.USTAR_FORMAT, mode='w', encoding='UTF-8') as out_archive:
                     # Sorting the file list ensures that we don't depend on the order that files appear on disk
                     for member in sorted(in_archive, key=lambda x: x.name):
                         # Clamping mtime to source_date_epoch ensures that source files are the only sources of timestamps, not build time
@@ -56,7 +64,14 @@ def normalize(filename, source_date_epoch=0):
 
                         fileobj = (in_archive.extractfile(member) if member.isfile() else None)
                         out_archive.addfile(member, fileobj)
+                if compress:
+                    archivefile.close()
             finally:
-                outfile.close()
-        os.utime(filename + '.tmp', (source_date_epoch, source_date_epoch))
-        os.rename(filename + '.tmp', filename)
+                if outfileobj is not None:
+                    outfile.close()
+        if outfileobj is None:
+            os.utime(filename + '.tmp', (source_date_epoch, source_date_epoch))
+            os.rename(filename + '.tmp', filename)
+
+    elif fileobj is not None and outfileobj is not None:
+        shutil.copyfileobj(fileobj, outfileobj)
