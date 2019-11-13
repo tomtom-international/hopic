tomtom-international/hopic

commit 2c474c26cbe30afed9e49aa5846cdb58beea704e

fix(carver): don't include the prerelease portion in tags by default
This is to avoid accidentally creating a tag for every prerelease which
is potentially every commit.

cidriver/versioning.py
@@ -244,7 +244,7 @@ class SemVer(object):
 class CarusoVer(object):
     """Caruso-specific versioning policy, overlaps with semantic versioning in syntax but definitely not compatible."""
     __slots__ = ('major', 'minor', 'patch', 'prerelease', 'increment', 'fix')
-    default_tag_name = '{version}'
+    default_tag_name = '{version.major}{version.minor}{version.patch}+PI{version.increment}.{version.fix}'
 
     def __init__(self, major, minor, patch, prerelease, increment, fix):
         super(CarusoVer, self).__init__()

commit 3204084c4851eacc386e04ffa1feaf99716ca209

fix(carver): separate the major, minor and patch components by dots
This fixes bug PIPE-358 which was introduced by
2c474c26cbe30afed9e49aa5846cdb58beea704e.

cidriver/versioning.py
@@ -244,7 +244,7 @@ class SemVer(object):
 class CarusoVer(object):
     """Caruso-specific versioning policy, overlaps with semantic versioning in syntax but definitely not compatible."""
     __slots__ = ('major', 'minor', 'patch', 'prerelease', 'increment', 'fix')
-    default_tag_name = '{version.major}{version.minor}{version.patch}+PI{version.increment}.{version.fix}'
+    default_tag_name = '{version.major}.{version.minor}.{version.patch}+PI{version.increment}.{version.fix}'
 
     def __init__(self, major, minor, patch, prerelease, increment, fix):
         super(CarusoVer, self).__init__()