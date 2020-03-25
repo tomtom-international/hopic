c52488b08fee2d267471a308323c0ab1d4e4e4f7
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2020-03-25 09:02:33 +0100

    feat: add configuration to upload artefacts on failed builds
    this allows exporting files that are being generated by the build that might help debugging what went wrong
    PIPE-460

--- vars/getCiDriver.groovy
+++ vars/getCiDriver.groovy
@@ -689,57 +689,64 @@ exec ssh -i '''
     }
   }
 
-  private def archive_artifacts_if_enabled(Map meta, String workspace) {
+  private void archive_artifacts_if_enabled(Map meta, String workspace, boolean error_occurred) {
     def archiving_cfg = meta.containsKey('archive') ? 'archive' : meta.containsKey('fingerprint') ? 'fingerprint' : null
-    if (archiving_cfg) {
-      def artifacts = meta[archiving_cfg].artifacts
-      if (artifacts == null) {
-        steps.error("Archive configuration entry for ${phase}.${variant} does not contain 'artifacts' property")
+    if (!archiving_cfg) {
+      return
+    }
+
+    def upload_on_failure = meta[archiving_cfg].getOrDefault('upload-on-failure', false)
+    if (!upload_on_failure && (error_occurred || steps.currentBuild.currentResult != 'SUCCESS')) {
+      return
+    }
+
+    def artifacts = meta[archiving_cfg].artifacts
+    if (artifacts == null) {
+      steps.error("Archive configuration entry for ${phase}.${variant} does not contain 'artifacts' property")
+    }
+    steps.dir(workspace) {
+      artifacts.each { artifact ->
+        def pattern = artifact.pattern.replace('(*)', '*')
+        if (archiving_cfg == 'archive') {
+          steps.archiveArtifacts(
+              artifacts: pattern,
+              fingerprint: meta.archive.getOrDefault('fingerprint', true),
+            )
+        } else if (archiving_cfg == 'fingerprint') {
+          steps.fingerprint(pattern)
+        }
       }
-      steps.dir(workspace) {
-        artifacts.each { artifact ->
-          def pattern = artifact.pattern.replace('(*)', '*')
-          if (archiving_cfg == 'archive') {
-            steps.archiveArtifacts(
-                artifacts: pattern,
-                fingerprint: meta.archive.getOrDefault('fingerprint', true),
-              )
-          } else if (archiving_cfg == 'fingerprint') {
-            steps.fingerprint(pattern)
-          }
+      if (meta[archiving_cfg].containsKey('upload-artifactory')) {
+        def server_id = meta[archiving_cfg]['upload-artifactory'].id
+        if (server_id == null) {
+          steps.error("Artifactory upload configuration entry for ${phase}.${variant} does not contain 'id' property to identify Artifactory server")
         }
-        if (meta[archiving_cfg].containsKey('upload-artifactory')) {
-          def server_id = meta[archiving_cfg]['upload-artifactory'].id
-          if (server_id == null) {
-            steps.error("Artifactory upload configuration entry for ${phase}.${variant} does not contain 'id' property to identify Artifactory server")
-          }
-          def uploadSpec = JsonOutput.toJson([
-              files: artifacts.collect { artifact ->
-                def fileSpec = [
-                  pattern: artifact.pattern,
-                  target: artifact.target,
-                ]
-                if (fileSpec.target == null) {
-                  steps.error("Artifactory upload configuration entry for ${phase}.${variant} does not contain 'target' property to identify target repository")
-                }
-                if (artifact.props != null) {
-                  fileSpec.props = artifact.props
-                }
-                return fileSpec
+        def uploadSpec = JsonOutput.toJson([
+            files: artifacts.collect { artifact ->
+              def fileSpec = [
+                pattern: artifact.pattern,
+                target: artifact.target,
+              ]
+              if (fileSpec.target == null) {
+                steps.error("Artifactory upload configuration entry for ${phase}.${variant} does not contain 'target' property to identify target repository")
               }
-            ])
-          if (!artifactoryBuildInfo.containsKey(server_id)) {
-            def newBuildInfo = steps.Artifactory.newBuildInfo()
-            def (build_name, build_identifier) = get_build_id()
-            newBuildInfo.name = build_name
-            newBuildInfo.number = build_identifier
-            artifactoryBuildInfo[server_id] = newBuildInfo
-          }
-          def server = steps.Artifactory.server server_id
-          server.upload(spec: uploadSpec, buildInfo: artifactoryBuildInfo[server_id])
-          // Work around Artifactory Groovy bug
-          server = null
+              if (artifact.props != null) {
+                fileSpec.props = artifact.props
+              }
+              return fileSpec
+            }
+          ])
+        if (!artifactoryBuildInfo.containsKey(server_id)) {
+          def newBuildInfo = steps.Artifactory.newBuildInfo()
+          def (build_name, build_identifier) = get_build_id()
+          newBuildInfo.name = build_name
+          newBuildInfo.number = build_identifier
+          artifactoryBuildInfo[server_id] = newBuildInfo
         }
+        def server = steps.Artifactory.server server_id
+        server.upload(spec: uploadSpec, buildInfo: artifactoryBuildInfo[server_id])
+        // Work around Artifactory Groovy bug
+        server = null
       }
     }
   }
@@ -880,6 +887,7 @@ exec ssh -i '''
                         returnStdout: true,
                       ))
 
+                    def error_occurred = false
                     try {
                       this.subcommand_with_credentials(
                           cmd,
@@ -887,6 +895,9 @@ exec ssh -i '''
                         + ' --phase=' + shell_quote(phase)
                         + ' --variant=' + shell_quote(variant)
                         , meta.getOrDefault('with-credentials', []))
+                    } catch(Exception e) {
+                      error_occurred = true // Jenkins only sets its currentResult to Failure after all user code is executed
+                      throw e
                     } finally {
                       if (meta.containsKey('junit')) {
                         def results = meta.junit
@@ -896,6 +907,7 @@ exec ssh -i '''
                           }
                         }
                       }
+                      this.archive_artifacts_if_enabled(meta, workspace, error_occurred)
                     }
 
                     // FIXME: re-evaluate if we can and need to get rid of special casing for stashing
@@ -942,7 +954,6 @@ exec ssh -i '''
                         )
                       this.worktree_bundles[name] = [nodes: [(steps.env.NODE_NAME): true]]
                     }
-                    this.archive_artifacts_if_enabled(meta, workspace)
                   }
                 }
               }]
