8d3d47da3b0659897bec1055cbf23908098b7f76
Giel van Schijndel <giel@mortis.eu>
2020-09-10 15:39:04 +0200

    refactor: move Hopic CLI entry point to separate module

--- hopic/cli/__init__.py
+++ hopic/cli/__init__.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import click
-import click_log
 
 from . import autocomplete
 from .utils import (
@@ -33,7 +32,6 @@ from .. import credentials
 from ..execution import echo_cmd
 from ..git_time import (
         determine_git_version,
-        determine_source_date,
         determine_version,
         restore_mtime_from_git,
     )
@@ -77,6 +75,8 @@ import sys
 import tempfile
 from typing import Final
 from textwrap import dedent
+
+from .main import main
 
 PACKAGE : Final[str] = __package__.split('.')[0]
 
@@ -187,181 +187,7 @@ class DockerContainers(object):
         self.containers.add(container_id)
 
 
-class OptionContext(object):
-    def __init__(self):
-        super().__init__()
-        self._opts = {}
-        self._missing_parameters = {}
-
-    def __getattr__(self, name):
-        if name in frozenset({'_opts', '_missing_parameters'}):
-            return super().__getattr__(name)
-
-        try:
-            return self._opts[name]
-        except KeyError:
-            pass
-        try:
-            missing_param = self._missing_parameters[name].copy()
-        except KeyError:
-            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'.")
-        else:
-            exception_raiser = missing_param.pop('exception_raiser')
-            exception_raiser(**missing_param)
-
-    def __setattr__(self, name, value):
-        if name in frozenset({'_opts', '_missing_parameters'}):
-            return super().__setattr__(name, value)
-
-        self._opts[name] = value
-
-    def __delattr__(self, name):
-        del self._opts[name]
-
-    def register_parameter(self, ctx, param, name=None, exception_raiser=None):
-        if name is None:
-            name = param.human_readable_name
-
-        if exception_raiser is None:
-            def exception_raiser(**kwargs):
-                raise click.MissingParameter(**kwargs)
-
-        self._missing_parameters[name] = dict(
-            ctx=ctx,
-            param=param,
-            exception_raiser=exception_raiser,
-        )
-
-    def register_dependent_attribute(self, name, dependency):
-        self._missing_parameters[name] = self._missing_parameters[dependency]
-
-
-@click.group(context_settings=dict(help_option_names=('-h', '--help')))
-@click.option('--color'          , type=click.Choice(('always', 'auto', 'never'))                                                  , default='auto'      , show_default=True)  # noqa: E501
-@click.option('--config'         , type=click.Path(exists=False, file_okay=True , dir_okay=False, readable=True, resolve_path=True), default=lambda: None, show_default='${WORKSPACE}/hopic-ci-config.yaml')  # noqa: E501
-@click.option('--workspace'      , type=click.Path(exists=False, file_okay=False, dir_okay=True)                                   , default=lambda: None, show_default='git work tree of config file or current working directory')  # noqa: E501
-@click.option('--whitelisted-var', multiple=True                                                                                   , default=['CT_DEVENV_HOME'], hidden=True)  # noqa: E501
-@click_log.simple_verbosity_option(PACKAGE                 , envvar='HOPIC_VERBOSITY', autocompletion=autocomplete.click_log_verbosity)
-@click_log.simple_verbosity_option('git', '--git-verbosity', envvar='GIT_VERBOSITY'  , autocompletion=autocomplete.click_log_verbosity)
-@click.pass_context
-def cli(ctx, color, config, workspace, whitelisted_var):
-    if color == 'always':
-        ctx.color = True
-    elif color == 'never':
-        ctx.color = False
-    else:
-        # leave as is: 'auto' is the default for Click
-        pass
-
-    click_log.basic_config()
-
-    ctx.obj = OptionContext()
-    for param in ctx.command.params:
-        ctx.obj.register_parameter(ctx=ctx, param=param)
-        if param.human_readable_name == 'workspace' and workspace is not None:
-            if ctx.invoked_subcommand != 'checkout-source-tree':
-                # Require the workspace directory to exist for anything but the checkout command
-                if not os.path.isdir(workspace):
-                    raise click.BadParameter(
-                        f"Directory '{workspace}' does not exist.",
-                        ctx=ctx, param=param
-                    )
-        elif param.human_readable_name == 'config' and config is not None:
-            # Require the config file to exist everywhere that it's used
-            try:
-                # Try to open the file instead of os.path.isfile because we want to be able to use /dev/null too
-                with open(config, 'rb'):
-                    pass
-            except IOError:
-                def exception_raiser(ctx, param):
-                    raise click.BadParameter(
-                        f"File '{config}' does not exist.",
-                        ctx=ctx, param=param
-                    )
-                ctx.obj.register_parameter(ctx=ctx, param=param, exception_raiser=exception_raiser)
-
-    if workspace is None:
-        # workspace default
-        if config is not None and config != os.devnull:
-            try:
-                with git.Repo(os.path.dirname(config), search_parent_directories=True) as repo:
-                    # Default to containing repository of config file, ...
-                    workspace = repo.working_dir
-            except (git.InvalidGitRepositoryError, git.NoSuchPathError):
-                # ... but fall back to containing directory of config file.
-                workspace = os.path.dirname(config)
-        else:
-            workspace = os.getcwd()
-    workspace = os.path.join(os.getcwd(), workspace)
-    ctx.obj.workspace = workspace
-
-    ctx.obj.volume_vars = {}
-    try:
-        with git.Repo(workspace) as repo, repo.config_reader() as cfg:
-            code_dir = os.path.join(workspace, cfg.get_value('hopic.code', 'dir'))
-    except (git.InvalidGitRepositoryError, git.NoSuchPathError, NoOptionError, NoSectionError):
-        code_dir = workspace
-
-    ctx.obj.code_dir = ctx.obj.volume_vars['WORKSPACE'] = code_dir
-    source_date = determine_source_date(code_dir)
-    if source_date is not None:
-        ctx.obj.source_date = source_date
-        ctx.obj.source_date_epoch = int((
-            source_date - datetime.utcfromtimestamp(0).replace(tzinfo=tzutc())
-        ).total_seconds())
-        ctx.obj.volume_vars['SOURCE_DATE_EPOCH'] = str(ctx.obj.source_date_epoch)
-    ctx.obj.register_dependent_attribute('code_dir', 'workspace')
-    ctx.obj.register_dependent_attribute('source_date', 'workspace')
-    ctx.obj.register_dependent_attribute('source_date_epoch', 'workspace')
-
-    for var in whitelisted_var:
-        try:
-            ctx.obj.volume_vars[var] = os.environ[var]
-        except KeyError:
-            pass
-
-    if config is not None:
-        ctx.obj.config_file = config
-    ctx.obj.register_dependent_attribute('config_file', 'config')
-    try:
-        config = determine_config_file_name(ctx)
-    except click.BadParameter:
-        config = None
-
-    cfg = {}
-    if config is not None:
-        if not os.path.isabs(config) and config != os.devnull:
-            config = os.path.join(os.getcwd(), config)
-        ctx.obj.volume_vars['CFGDIR'] = ctx.obj.config_dir = os.path.dirname(config)
-        # Prevent reading the config file _before_ performing a checkout. This prevents a pre-existing file at the same
-        # location from being read as the config file. This may cause problems if that pre-checkout file has syntax
-        # errors for example.
-        if ctx.invoked_subcommand != 'checkout-source-tree':
-            try:
-                # Try to open the file instead of os.path.isfile because we want to be able to use /dev/null too
-                with open(config, 'rb'):
-                    pass
-            except IOError:
-                pass
-            else:
-                cfg = ctx.obj.config = read_config(config, ctx.obj.volume_vars)
-    ctx.obj.register_dependent_attribute('config_dir', 'config')
-
-    ctx.obj.version = determine_version(
-            cfg.get('version', {}),
-            config_dir=(config and ctx.obj.config_dir),
-            code_dir=ctx.obj.code_dir,
-        )
-    if ctx.obj.version is not None:
-        log.debug("read version: \x1B[34m%s\x1B[39m", ctx.obj.version)
-        ctx.obj.volume_vars['VERSION'] = str(ctx.obj.version)
-        ctx.obj.volume_vars['PURE_VERSION'] = ctx.obj.volume_vars['VERSION'].split('+')[0]
-        # FIXME: make this conversion work even when not using SemVer as versioning policy
-        # Convert SemVer to Debian version: '~' for pre-release instead of '-'
-        ctx.obj.volume_vars['DEBVERSION'] = ctx.obj.volume_vars['VERSION'].replace('-', '~', 1).replace('.dirty.', '+dirty', 1)
-
-
-@cli.command()
+@main.command()
 @click.pass_context
 def may_publish(ctx):
     """
@@ -495,7 +321,7 @@ def to_git_time(date):
     return f"{utctime} {date:%z}"
 
 
-@cli.command()
+@main.command()
 @click.option('--target-remote'     , metavar='<url>')
 @click.option('--target-ref'        , metavar='<ref>')
 @click.option('--clean/--no-clean'  , default=False, help='''Clean workspace of non-tracked files''')
@@ -579,7 +405,7 @@ def checkout_source_tree(ctx, target_remote, target_ref, clean, ignore_initial_s
                   clean, clean_config=ctx.obj.config['clean'])
 
 
-@cli.group()
+@main.group()
 # git
 @click.option('--author-name'               , metavar='<name>'                 , help='''Name of change-request's author''')
 @click.option('--author-email'              , metavar='<email>'                , help='''E-mail address of change-request's author''')
@@ -1153,7 +979,7 @@ def bump_version(ctx):
     return change_applicator
 
 
-@cli.command()
+@main.command()
 @click.option('--phase'             , metavar='<phase>'  , multiple=True, help='''Build phase''', autocompletion=autocomplete.phase_from_config)
 @click.option('--variant'           , metavar='<variant>', multiple=True, help='''Configuration variant''', autocompletion=autocomplete.variant_from_config)
 @click.pass_context
@@ -1200,7 +1026,7 @@ def getinfo(ctx, phase, variant):
     click.echo(json.dumps(info, indent=4, separators=(',', ': '), cls=JSONEncoder))
 
 
-@cli.command()
+@main.command()
 @click.option('--phase'  , metavar='<phase>'  , multiple=True, help='''Build phase to execute''', autocompletion=autocomplete.phase_from_config)
 @click.option('--variant', metavar='<variant>', multiple=True, help='''Configuration variant to build''', autocompletion=autocomplete.variant_from_config)
 @click.pass_context
@@ -1559,7 +1385,7 @@ def build(ctx, phase, variant):
                     binary_normalize.normalize(os.path.join(ctx.obj.code_dir, artifact), source_date_epoch=ctx.obj.source_date_epoch)
 
 
-@cli.command()
+@main.command()
 @click.option('--bundle', metavar='<file>', help='Git bundle to use', type=click.Path(file_okay=True, dir_okay=False, readable=True, resolve_path=True))
 @click.pass_context
 def unbundle_worktrees(ctx, bundle):
@@ -1606,7 +1432,7 @@ def unbundle_worktrees(ctx, bundle):
             cfg.set_value(section, 'refspecs', ' '.join(shlex.quote(refspec) for refspec in refspecs))
 
 
-@cli.command()
+@main.command()
 @click.option('--target-remote', metavar='<url>', help='''The remote to push to, if not specified this will default to the checkout remote.''')
 @click.pass_context
 def submit(ctx, target_remote):
@@ -1627,7 +1453,7 @@ def submit(ctx, target_remote):
             cfg.remove_section(section)
 
 
-@cli.command()
+@main.command()
 @click.pass_context
 def show_config(ctx):
     """
@@ -1637,7 +1463,7 @@ def show_config(ctx):
     click.echo(json.dumps(ctx.obj.config, indent=4, separators=(',', ': '), cls=JSONEncoder))
 
 
-@cli.command()
+@main.command()
 @click.pass_context
 def show_env(ctx):
     """
