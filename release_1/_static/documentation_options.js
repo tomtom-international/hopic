fd38c07165f0261d1c49cf0fdc7eb5f335dbc251
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2020-10-02 14:36:07 +0000

    Update documentation for 1.21.3-19+g151e470

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.21.3.dev17+g479ef72',
+    VERSION: '1.21.3.dev19+g151e470',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
