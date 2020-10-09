483f45e698e13da9186b68f6f693e82ff113ffe6
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2020-10-09 10:09:57 +0000

    Update documentation for 1.21.3-21+g254adfc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.21.3.dev92+g8f9c6b8',
+    VERSION: '1.21.3.dev21+g254adfc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
