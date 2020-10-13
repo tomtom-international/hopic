f8287dcebea5130d7e6add5e7585bfb5f1e10d95
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2020-10-13 12:18:50 +0000

    Update documentation for 1.22.1-6+gf91ef8f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.22.1.dev2+g882d68a',
+    VERSION: '1.22.1.dev6+gf91ef8f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
