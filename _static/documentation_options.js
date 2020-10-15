d1a269dbffa3c0d597a61108e3e0688af663098e
CI System <no-reply@tomtom.com>
2020-10-15 16:57:58 +0000

    Update documentation for 1.22.1-86+ga9f0cea

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.22.1.dev9+g5336584',
+    VERSION: '1.22.1.dev86+ga9f0cea',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
