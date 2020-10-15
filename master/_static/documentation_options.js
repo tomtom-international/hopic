89d3286a70469145266c305654c68130054683ad
Joost Muller <Joost.Muller@tomtom.com>
2020-10-15 11:58:40 +0000

    Update documentation for 1.22.1-9+g5336584

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.22.1.dev82+g6b6bb49',
+    VERSION: '1.22.1.dev9+g5336584',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
