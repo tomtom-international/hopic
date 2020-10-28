16cf0ef23e4c0fb6720355b91ff72aded0b9e0bd
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2020-10-28 13:20:59 +0000

    Update documentation for 1.22.1-37+g1b1a265

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.22.1.dev117+g27db9f3',
+    VERSION: '1.22.1.dev37+g1b1a265',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
