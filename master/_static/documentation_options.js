9647529156d2e1fa195fa63212624948607709d2
CI System <no-reply@tomtom.com>
2020-11-02 08:57:58 +0000

    Update documentation for 1.22.1-124+g03d077c

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.22.1.dev39+g7d2ba27',
+    VERSION: '1.22.1.dev124+g03d077c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
