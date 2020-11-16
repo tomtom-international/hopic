eb30f19a903e81361adf0b8d8ca17c6c6b0df2f6
James Weir <James.Weir@tomtom.com>
2020-11-16 17:44:04 +0000

    Update documentation for 1.24.1-5+ga6bc5ec

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.24.1.dev90+g4ab45bb',
+    VERSION: '1.24.1.dev5+ga6bc5ec',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
