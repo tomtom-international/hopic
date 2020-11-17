96b826c2c5f840e061f6f11ab60fec50f0ea2e04
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2020-11-17 12:30:33 +0000

    Update documentation for 1.24.1-25+g1c6b6d7

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.24.1.dev7+g2ae9c0a',
+    VERSION: '1.24.1.dev25+g1c6b6d7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
