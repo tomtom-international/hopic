8f78bce7669933dff6f1050537a8f2a19ee7952e
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2020-12-03 13:52:58 +0000

    Update documentation for 1.26.1-20+gcf08cf20

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.26.1.dev17+g7effe178',
+    VERSION: '1.26.1.dev20+gcf08cf20',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
