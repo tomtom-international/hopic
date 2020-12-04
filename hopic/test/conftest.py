d89dbda11c344004e71c83f0951379ca945d8d09
Giel van Schijndel <giel@mortis.eu>
2020-12-04 13:46:03 +0100

    test(doc): examples used in the documentation are syntactically valid

--- hopic/test/conftest.py
+++ hopic/test/conftest.py
@@ -12,8 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ..cli import utils
-import pytest
+import os.path
+from pathlib import (
+    Path,
+    PurePath,
+)
+import typing
 
 try:
     # Python >= 3.8
@@ -21,8 +25,14 @@ try:
 except ImportError:
     import importlib_metadata as metadata
 
-PACKAGE : str = __package__.split('.')[0]
+import pytest
+
+from ..cli import utils
+
+PACKAGE: str = __package__.split('.')[0]
 _hopic_version_str = f"{PACKAGE}=={metadata.version(PACKAGE)}"
+_root_dir = Path(__file__).parent / '..' / '..'
+_example_dir = _root_dir / 'examples'
 
 
 @pytest.fixture(autouse=True)
@@ -31,3 +41,55 @@ def pip_freeze_constant(monkeypatch):
     with monkeypatch.context() as m:
         m.setattr(utils, 'installed_pkgs', lambda: _hopic_version_str)
         yield m
+
+
+def _data_file_paths(
+    datadir: typing.Union[str, PurePath],
+    *,
+    suffices: typing.Optional[typing.AbstractSet[str]] = None,
+):
+    for entry in datadir.iterdir():
+        if suffices is not None and entry.suffix not in suffices:
+            continue
+
+        if entry.is_dir() or entry.is_socket() or entry.is_block_device():
+            continue
+
+        yield entry
+
+
+def _data_file_path_id(
+    datadir: typing.Union[str, PurePath],
+    name,
+):
+    try:
+        return os.path.relpath(name, datadir)
+    except TypeError:
+        return str(name)
+
+
+@pytest.fixture(
+    params=_data_file_paths(_example_dir, suffices={'.yml', '.yaml'}),
+    ids=lambda entry: _data_file_path_id(_example_dir, entry),
+)
+def example_file(request):
+    with open(request.param, 'r') as f:
+        yield f
+
+
+@pytest.fixture(
+    params=_data_file_paths(_example_dir / 'simple', suffices={'.yml', '.yaml'}),
+    ids=lambda entry: _data_file_path_id(_example_dir, entry),
+)
+def simple_example_file(request):
+    with open(request.param, 'r') as f:
+        yield f
+
+
+@pytest.fixture(
+    params=_data_file_paths(_example_dir / 'embed', suffices={'.yml', '.yaml'}),
+    ids=lambda entry: _data_file_path_id(_example_dir, entry),
+)
+def embed_example_file(request):
+    with open(request.param, 'r') as f:
+        yield f
