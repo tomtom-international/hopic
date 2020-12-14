c9f294ab5d79576bd79ef70ef9c57e3c7a38a621
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2020-12-14 11:23:48 +0000

    Update documentation for 1.27.1+ga5c8742

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.27.1.dev9+ga5c8742',
+    VERSION: '1.27.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
