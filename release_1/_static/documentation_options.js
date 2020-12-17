808a5a02a1a073894ac3676eb5ebe28536568efd
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2020-12-17 14:24:30 +0000

    Update documentation for 1.27.2-12+gefa3db5

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.27.2.dev119+ga9fdedf1',
+    VERSION: '1.27.2.dev12+gefa3db5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
