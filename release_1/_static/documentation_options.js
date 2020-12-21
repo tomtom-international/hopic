c21264aa6aeb8316cf8fb4fa42f8225ef5dd25c2
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2020-12-21 11:06:34 +0000

    Update documentation for 1.27.2-14+gf8e4f9f

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.27.2.dev122+g94b884c',
+    VERSION: '1.27.2.dev14+gf8e4f9f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
