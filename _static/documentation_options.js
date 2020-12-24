f583ace60d9cefa854a48459c4a3acace688f55d
CI System <no-reply@tomtom.com>
2020-12-24 11:31:39 +0000

    Update documentation for 1.27.2-153+gb928fbb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.27.2.dev38+gb78849c',
+    VERSION: '1.27.2.dev153+gb928fbb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
