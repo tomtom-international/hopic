00bf4b683c54773896bf11a1bec350fa2a1307de
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2021-01-05 17:07:26 +0000

    Update documentation for 1.29.2-5+ga4f9993

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.29.2.dev131+g1c595d41',
+    VERSION: '1.29.2.dev5+ga4f9993',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
