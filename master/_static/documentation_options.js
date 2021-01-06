4e9ab166674994a8d846e936ee483d6714266e2f
Giel van Schijndel <giel@mortis.eu>
2021-01-06 16:21:50 +0100

    Update documentation for 1.29.2+g0501fbc

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.29.2.dev9+gdc4a539b',
+    VERSION: '1.29.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
