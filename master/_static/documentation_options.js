31dbd1198dfa4045716b8ea94389910c81430494
Joost Muller <Joost.Muller@tomtom.com>
2021-01-21 19:53:53 +0000

    Update documentation for 1.39.3-8+g4a7138a8a8d031

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.39.3.dev149+gfebaa2e',
+    VERSION: '1.39.3.dev8+g4a7138a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
