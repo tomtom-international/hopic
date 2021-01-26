77bbbce25d588998a694fb10bf653636a80e1546
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2021-01-26 16:43:10 +0000

    Update documentation for 1.30.1-7+gdf886743

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.30.1.dev3+g064e2f1',
+    VERSION: '1.30.1.dev7+gdf886743',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
