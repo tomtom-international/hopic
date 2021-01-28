f9e1eb2773e4ba9d8a465f89a834699bcad44dda
Joost Muller <Joost.Muller@tomtom.com>
2021-01-28 10:52:44 +0000

    Update documentation for 1.31.0+g3a5fad60

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.30.1.dev23+g3a5fad60',
+    VERSION: '1.31.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
