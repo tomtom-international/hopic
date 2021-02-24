61964b1b3ce1215a25b695da3b1ec84e04e9a7ce
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2021-02-24 21:19:48 +0000

    Update documentation for 1.33.2+g5d1fa9c9411987

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.33.2.dev5+g5d1fa9c',
+    VERSION: '1.33.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
