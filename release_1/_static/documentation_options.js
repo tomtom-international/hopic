3a67f0ad0af29cf08f62cf63bd511fb6871453db
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2021-05-21 10:41:21 +0000

    Update documentation for 1.40.2-12+gb08cb9188a279b

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.40.2.dev153+g9506b57',
+    VERSION: '1.40.2.dev12+gb08cb91',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
