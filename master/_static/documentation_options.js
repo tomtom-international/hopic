1985feed80726d5ffcd38c0a9e3c4e596ef8ec0f
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2021-06-07 09:23:06 +0000

    Update documentation for 1.40.3-7+g6766588a4be549

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.40.3.dev156+gaa5aabd',
+    VERSION: '1.40.3.dev7+g6766588',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
