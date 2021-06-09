f4921d696339b4cabd9c04e94f41e795f50ee0b8
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2021-06-09 12:57:02 +0000

    Update documentation for 1.40.3-19+gbd199eb2e30f9d

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.40.3.dev17+ga5dc15c',
+    VERSION: '1.40.3.dev19+gbd199eb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
