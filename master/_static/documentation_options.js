133635dd59c7931500d1d3bd05501875abf7e78d
Giel van Schijndel <giel@mortis.eu>
2021-07-16 17:20:12 +0200

    Update documentation for 1.42.0+g7b3a2079405580

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.41.4.dev14+g2eacca0b',
+    VERSION: '1.42.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
