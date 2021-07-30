38b7ae3d6b33a66027523160e26486c9421f2666
Maikel van den Hurk <Maikel.vandenHurk@tomtom.com>
2021-07-30 10:05:14 +0000

    Update documentation for 1.43.1+g0905918a576850

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.43.1.dev2+g0905918a',
+    VERSION: '1.43.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
