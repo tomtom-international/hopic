c247ee006ff63cb6adf05d6a195fe046a44ae762
Joost Muller <Joost.Muller@tomtom.com>
2021-08-19 11:18:54 +0000

    Update documentation for 1.45.0+g4ecd218a10aa78

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.44.1.dev11+g4ecd218a',
+    VERSION: '1.45.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
