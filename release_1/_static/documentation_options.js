ce49b77134ea867e2b7ebed8df4fd8c184ec4f52
Giel van Schijndel <giel@mortis.eu>
2021-10-06 16:54:55 +0200

    Update documentation for 1.48.0+g839ac27a3e8f3d

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.47.1.dev182+g7b0f760c',
+    VERSION: '1.48.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
