5a909333f5cb19d0791c04aeb4a1d7d578d134f4
martijn leijssen <martijn.leijssen@tomtom.com>
2021-10-20 17:45:46 +0200

    fix: report build status on merge commit rather than target commit

--- vars/getCiDriver.groovy
+++ vars/getCiDriver.groovy
@@ -931,6 +931,7 @@ SSH_ASKPASS_REQUIRE=force SSH_ASKPASS='''
             + shell_quote("${tmpdir}/${this.change_bundle}"),
           label: 'Hopic (internal): unbundle change',
         )
+        return this.submit_info.commit
       } else if (this.get_change() != null) {
         def meta = this.get_change().getinfo(cmd)
         def maybe_timeout = { Closure closure ->
