a425ad1957dc8426b95fa10e13333f56d71e465f
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2021-12-16 16:25:29 +0000

    Update documentation for 1.50.0+g35d4eeab077ce4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.49.3.dev4+g35d4eeab',
+    VERSION: '1.50.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
