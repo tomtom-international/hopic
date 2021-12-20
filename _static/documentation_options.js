e5d6a238e185d440c7fc78cf2bedce50114e4a6b
Maikel van den Hurk <Maikel.vandenHurk@tomtom.com>
2021-12-20 08:37:48 +0000

    Update documentation for 1.50.1-2+g40a1913d4b97f7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.50.0',
+    VERSION: '1.50.1.dev2+g40a1913d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
