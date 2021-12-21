59890a4788ed39c7f37101a0c64a7059b6d09e3a
Maikel van den Hurk <Maikel.vandenHurk@tomtom.com>
2021-12-21 14:15:06 +0000

    Update documentation for 1.50.1-8+gb908379f1d8c6d

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.50.1.dev6+g51a23cbe',
+    VERSION: '1.50.1.dev8+gb908379f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
