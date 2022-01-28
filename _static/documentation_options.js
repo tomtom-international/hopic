7d7a6fbbaa1d9a03f0abb15e64013fe8298c2da6
CI System <no-reply@tomtom.com>
2022-01-28 10:30:39 +0000

    Update documentation for 1.53.0+g0bc0d37f09cdc9

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.52.1.dev199+gce463fd1',
+    VERSION: '1.53.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
