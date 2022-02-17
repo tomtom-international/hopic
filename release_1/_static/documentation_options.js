fe6b4f7ff326467122729aab45f36687292ccb01
Andrey Potapov <Andrey.Potapov@tomtom.com>
2022-02-17 10:49:10 +0000

    Update documentation for 1.54.0+g7491d8ec22267f

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.53.1.dev215+g18bbdeb4',
+    VERSION: '1.54.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
