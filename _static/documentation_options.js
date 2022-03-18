72f8303b4fb043657af11de3abe0ef4edeeea910
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2022-03-18 16:12:53 +0000

    Update documentation for 1.54.1-4+gb6fa360211cde6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.54.1.dev200+g83012aa9',
+    VERSION: '1.54.1.dev4+gb6fa3602',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
