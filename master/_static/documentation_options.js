62ef2c99a1e0cbd994d381104cb837e80f2a04f0
Alexandru Lazarescu <Alexandru.Lazarescu@tomtom.com>
2022-03-23 12:49:50 +0000

    Update documentation for 1.54.1-20+g6a76f6b034a60b

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.54.1.dev18+g306f9766',
+    VERSION: '1.54.1.dev20+g6a76f6b0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
