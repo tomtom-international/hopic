4c50620dddb0048ff906715ab7774137533aba86
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2022-06-29 09:43:18 +0000

    Update documentation for 1.56.2-2+g9329fccc1e6911

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.56.1',
+    VERSION: '1.56.2.dev2+g9329fccc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
