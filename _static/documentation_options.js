bc2d3fce8e17ad4e2de1826f4aec7853830d68d7
Giel van Schijndel <Giel.vanSchijndel@tomtom.com>
2022-07-04 11:17:11 +0000

    Update documentation for 1.57.0+ge51bc33a455012

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.56.3.dev5+ge51bc33a',
+    VERSION: '1.57.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
