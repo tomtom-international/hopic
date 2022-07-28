fec4b56bbc8a841c567b2ebe4ddd589bf953c44a
Martijn Leijssen <Martijn.Leijssen@tomtom.com>
2022-07-28 12:30:31 +0000

    Update documentation for 1.57.2+g649ed05d578e9b

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.57.2.dev3+g649ed05d',
+    VERSION: '1.57.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
