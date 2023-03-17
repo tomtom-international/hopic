9ca6eca06d0e6d4e0c4cd8a7b298b768d395998c
Joost Muller <Joost.Muller@tomtom.com>
2023-03-17 09:47:41 +0000

    Update documentation for 1.59.1-9+g699932e2753584

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.59.1.dev7+g698279ac',
+    VERSION: '1.59.1.dev9+g699932e2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
