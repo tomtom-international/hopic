e2e02eb025c136143c632ab90b3f05dd327867fa
Giel van Schijndel <giel@mortis.eu>
2023-05-26 15:13:45 +0200

    Update documentation for 1.61.3+g1bb149df5d2bfb

--- release_1/_static/documentation_options.js
+++ release_1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.61.3.dev212+g6392ac5f',
+    VERSION: '1.61.3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
